8f8ddf354c2759d91891cd3773bf0ebb2c9ca12c
jinkyoungun <jinkyoungun@naver.com>
2026-09-08 10:32:29 +0900

    Apply final metallic Artist Training app icon

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='artist-training-v2-3';
+const CACHE='artist-training-v2-4';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./progress-addon.css','./progress-addon.js','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
