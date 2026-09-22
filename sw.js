ab8a347ffb8c19601c491de127be5072b2c7cb35
jinkyoungun <jinkyoungun@naver.com>
2026-09-22 10:33:17 +0900

    Refresh cache for backup and new start date

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='artist-training-final-20260922-backup1';
+const CACHE='artist-training-final-20260922-backup-startreset1';
 const ASSETS=[
   './index.html',
   './manifest.webmanifest',
